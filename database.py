ec3fbacea12c089c6f75f24c32dfb965b1f3fff9
Om Priya <Om.Priya@watchguard.com>
2023-10-12 15:08:03 +0530

    singleton db class added as comment

--- database.py
+++ database.py
@@ -16,3 +16,34 @@ class DatabaseConnection:
         else:
             self.connection.commit()
             self.connection.close()
+
+
+"""
+class SingletonDatabse:
+    __instance = None
+
+    @staticmethod
+    def get_instance(host):
+        if SingletonDatabse.__instance == None:
+            SingletonDatabse(host)
+        return SingletonDatabse.__instance
+
+    def __init__(self, host):
+        if SingletonDatabse.__instance != None:
+            raise Exception("Don't create the object create using get_instance()")
+        else:
+            SingletonDatabse.__instance = self
+            self.connection = None
+            self.host = host
+
+    def __enter__(self):
+        self.connection = sqlite3.connect(self.host)
+        return self.connection
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type or exc_tb or exc_val:
+            self.connection.close()
+        else:
+            self.connection.commit()
+            self.connection.close()
+"""
