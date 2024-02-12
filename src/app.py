341983353b35979e5e326f97a96cac349df79f41
Om Priya <ompriya18153789@gmail.com>
2024-02-12 16:42:47 +0530

    Updated app.py

--- src/app.py
+++ src/app.py
@@ -3,8 +3,16 @@ from router.public_routes.product_router import product_router
 from router.public_routes.auth_router import auth_router
 from router.private_routes.admin_sales_router import sales_router
 from router.private_routes.admin_product_router import admin_product_router
+from config.product_query import ProductQuery
+from config.user_query import UserQuery
+from config.transaction_query import TransactionQuery
+from database.db_access import DbAccess
 
 app = FastAPI()
+
+DbAccess.write_to_database(ProductQuery.CREATE_PRODUCT_TABLE)
+DbAccess.write_to_database(UserQuery.CREATE_USER_TABLE)
+DbAccess.write_to_database(TransactionQuery.CREATE_TABLE)
 
 app.include_router(product_router)
 app.include_router(auth_router)
